fd95493b1f05caad537d088d87b4067cc77d6b91
Vincent Manz <vincentmanz@aol.com>
2024-12-12 17:17:04 +0100

    Added tempalte + flask stuff

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 import factorio_rcon
 from dotenv import load_dotenv
 import os
+from flask import Flask, render_template
 
 load_dotenv()
 ip = os.getenv('IP')
 rcon_password = os.getenv('RCON_PASSWORD')
+client = factorio_rcon.RCONClient(ip, 27015, rcon_password)
 
-while True:
-    client = factorio_rcon.RCONClient(ip, 27015, rcon_password)
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
     response = client.send_command("/players online")
-    print(response)
+    response = response.replace("(online)"," ")
+    response = response.split("\n")
+    return render_template('player_template', text=response)
+
+    
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 8080))
+    app.run(debug=False, host='0.0.0.0', port=port)
 
 
